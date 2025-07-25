1a2e332bee42fe53623365e33d96da9c96f19722
DorofeevDaniil <dorofeefdi@gmail.com>
2025-07-25 21:09:46 +0300

    split mapper

--- src/main/java/ru/custom/intershop/mapper/CartMapper.java
+++ src/main/java/ru/custom/intershop/mapper/CartMapper.java
@@ -11,38 +11,14 @@ public class CartMapper {
     private CartMapper() {}
 
     public static CartDto toCartDto(Cart cart) {
-        List<ItemDto> items = cart.getItems().stream().map(CartMapper::toItemDto).toList();
+        List<ItemDto> items = cart.getItems().stream().map(ItemMapper::toItemDto).toList();
 
         return new CartDto(items, cart.getTotal());
     }
 
-    public static ItemDto toItemDto(Item item) {
-        ItemDto itemDto = new ItemDto();
-        itemDto.setId(item.getId());
-        itemDto.setTitle(item.getTitle());
-        itemDto.setDescription(item.getDescription());
-        itemDto.setImgPath(item.getImgPath());
-        itemDto.setPrice(item.getPrice());
-        itemDto.setCount(item.getCount());
-
-        return itemDto;
-    }
-
     public static Cart toCart(CartDto cartDto) {
-        List<Item> items = cartDto.getItems().stream().map(CartMapper::toItem).toList();
+        List<Item> items = cartDto.getItems().stream().map(ItemMapper::toItem).toList();
 
         return new Cart(items, cartDto.getTotal());
-    }
-
-    public static Item toItem(ItemDto itemDto) {
-        Item item = new Item();
-        item.setId(itemDto.getId());
-        item.setTitle(itemDto.getTitle());
-        item.setDescription(itemDto.getDescription());
-        item.setImgPath(itemDto.getImgPath());
-        item.setPrice(itemDto.getPrice());
-        item.setCount(itemDto.getCount());
-
-        return item;
     }
 }
